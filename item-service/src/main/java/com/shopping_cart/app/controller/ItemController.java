920f7be47e50a3a6cfa32910af236a236b54c0d7
rohit-56 <rohitmou25@gmail.com>
2026-03-03 23:56:22 +0530

    update item-service mapping and port

--- item-service/src/main/java/com/shopping_cart/app/controller/ItemController.java
+++ item-service/src/main/java/com/shopping_cart/app/controller/ItemController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/items")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ItemController implements Serializable {
 
