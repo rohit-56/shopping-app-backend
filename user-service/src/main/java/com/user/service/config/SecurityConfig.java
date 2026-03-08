3a79935a9788b6036ccb2ae6fa661f4afdf4c623
rohit-56 <rohitmou25@gmail.com>
2026-03-08 16:25:22 +0530

    add required dependencies and properties for distributed tracing

--- user-service/src/main/java/com/user/service/config/SecurityConfig.java
+++ user-service/src/main/java/com/user/service/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.user.service.config;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    ObservationRegistry observationRegistry() {
+        return ObservationRegistry.create();
+    }
+
+
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
 
     @Bean
      public SecurityFilterChain configure(HttpSecurity http) throws Exception {
