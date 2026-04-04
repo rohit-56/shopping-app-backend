52b1203f61a1128ba39b6cdb25daf6e131ea4127
rohit-56 <rohitmou25@gmail.com>
2026-04-04 09:47:27 +0530

    fix search by category issue

--- item-service/src/main/java/com/shopping_cart/app/repository/ItemEntityRepository.java
+++ item-service/src/main/java/com/shopping_cart/app/repository/ItemEntityRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ItemEntityRepository extends JpaRepository<ItemEntity, Long> {
 
-  List<ItemEntity> findByCategoryType(CategoryType categoryType);
+  List<ItemEntity> findByCategoryType(String category);
 }
